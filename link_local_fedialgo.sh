523448a7d54b7d0c84935d72f5b8b5adea5a27db
lens0021 <lorentz0021@gmail.com>
2025-12-31 11:37:29 +0900

    Remove shell scripts

--- link_local_fedialgo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Linking local fedialgo package..."
-pushd ../fedialgo
-npm link
-npm run build
-popd
-npm link fedialgo
--- quick_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Simple script to run the setup for the fedialgo demo app.
-git clone https://github.com/michelcrypt4d4mus/fedialgo_demo_app_foryoufeed
-cd fedialgo_demo_app_foryoufeed
-npm install
-echo -e "\n\n\nYou can ignore any warnings above but not errors."
-echo -e "\nRun 'cd fedialgo_demo_app_foryoufeed && npm run start' to start the app server, then point your browser at http://localhost:3000/"
