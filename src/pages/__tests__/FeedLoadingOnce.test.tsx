a3876b700b14af659273f7d02c9606d3830dbff2
lens0021 <lorentz0021@gmail.com>
2026-01-04 01:20:52 +0900

    chore: format feed loading test imports

--- src/pages/__tests__/FeedLoadingOnce.test.tsx
+++ src/pages/__tests__/FeedLoadingOnce.test.tsx
@@ -1,4 +1,10 @@
-import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
+import {
+	act,
+	fireEvent,
+	render,
+	screen,
+	waitFor,
+} from "@testing-library/react";
 import { vi } from "vitest";
 
 import { config } from "../../config";
